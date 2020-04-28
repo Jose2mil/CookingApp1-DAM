3b0b7d79355dfc4297105a02d699047d344eb152
Pablo <pablonisen@gmail.com>
2020-04-28 10:19:17 +0200

    Adding Ingredient class and data to pantry screen
    Ingredient list is loaded to the pantry screen

---
+++ Stage_02/CookApp/src/myClasses/Ingredient.java
@@ -0,0 +1,75 @@
+package myClasses;
+
+public class Ingredient implements Comparable<Ingredient>
+{
+    String name;
+    short amount;
+    String unitOfMeasurement;
+    float kcal;
+    float lipidFats;
+    float saturatedFats;
+    float carbohydrates;
+    float sugars;
+    float protein;
+    float fibre;
+    float salt;
+
+    public Ingredient(String name, short amount,
+                      String unitOfMeasurement, float kcal,
+                      float lipidFats, float saturatedFats,
+                      float carbohydrates, float sugars,
+                      float protein, float fibre, float salt)
+    {
+        this.name = name;
+        this.amount = amount;
+        this.unitOfMeasurement = unitOfMeasurement;
+        this.kcal = kcal;
+        this.lipidFats = lipidFats;
+        this.saturatedFats = saturatedFats;
+        this.carbohydrates = carbohydrates;
+        this.sugars = sugars;
+        this.protein = protein;
+        this.fibre = fibre;
+        this.salt = salt;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public short getAmount() { return amount; }
+    public void setAmount(short amount) { this.amount = amount; }
+    public String getUnitOfMeasurement() { return unitOfMeasurement; }
+    public void setUnitOfMeasurement(String unitOfMeasurement) { this.unitOfMeasurement = unitOfMeasurement; }
+    public float getKcal() { return kcal; }
+    public void setKcal(float kcal) { this.kcal = kcal; }
+    public float getLipidFats() { return lipidFats; }
+    public void setLipidFats(float lipidFats) { this.lipidFats = lipidFats; }
+    public float getSaturatedFats() { return saturatedFats; }
+    public void setSaturatedFats(float saturatedFats) { this.saturatedFats = saturatedFats; }
+    public float getCarbohydrates() { return carbohydrates; }
+    public void setCarbohydrates(float carbohydrates) { this.carbohydrates = carbohydrates; }
+    public float getSugars() { return sugars; }
+    public void setSugars(float sugars) { this.sugars = sugars; }
+    public float getProtein() { return protein; }
+    public void setProtein(float protein) { this.protein = protein; }
+    public float getFibre() { return fibre; }
+    public void setFibre(float fibre) { this.fibre = fibre; }
+    public float getSalt() { return salt; }
+    public void setSalt(float salt) { this.salt = salt; }
+
+    @Override
+    public String toString()
+    {
+        return name + "  " + amount + " " + unitOfMeasurement;
+    }
+
+    @Override
+    public int compareTo(Ingredient i)
+    {
+        return this.getName().compareTo(i.getName());
+    }
+
+    public void modifyAmount(short amount)
+    {
+        //TODO
+    }
+}
--- Stage_02/CookApp/src/pantry/pantryController.java
+++ Stage_02/CookApp/src/pantry/pantryController.java
@@ -1,22 +1,66 @@
 package pantry;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+import myClasses.Ingredient;
 
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class pantryController implements Initializable
 {
+    public ListView listPantry;
+    public Label lbKcal;
+    public Label lbSugars;
+    public Label lbFat;
+    public Label lbFibre;
+    public Label lbSaturates;
+    public Label lbProtein;
+    public Label lbCarbs;
+    public Label lbSalt;
+    ObservableList<Ingredient> products;
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        products = FXCollections.observableArrayList(readIngredients());
+        listPantry.setItems(products.sorted());
 
+        listPantry.getSelectionModel().selectedItemProperty().addListener(
+                new ChangeListener<Ingredient>()
+                {
+                    @Override
+                    public void changed(ObservableValue<? extends Ingredient> observableValue,
+                                        Ingredient ing, Ingredient ing1) {
+                        {
+                            lbKcal.setText(String.valueOf(ing1.getKcal()));
+                            lbSugars.setText(String.valueOf(ing1.getSugars()));
+                            lbFat.setText(String.valueOf(ing1.getLipidFats()));
+                            lbFibre.setText(String.valueOf(ing1.getFibre()));
+                            lbSaturates.setText(String.valueOf(ing1.getSaturatedFats()));
+                            lbProtein.setText(String.valueOf(ing1.getProtein()));
+                            lbCarbs.setText(String.valueOf(ing1.getCarbohydrates()));
+                            lbSalt.setText(String.valueOf(ing1.getSalt()));
+                        }
+                    }
+                }
+        );
     }
 
     public void backToMainWindow(ActionEvent actionEvent) throws IOException {
@@ -27,5 +71,30 @@ public class pantryController implements Initializable
         app_stage.hide();
         app_stage.setScene(home_page_scene);
         app_stage.show();
+    }
+
+    private List<Ingredient> readIngredients() {
+
+        try
+        {
+            List<String> lines = Files.readAllLines(Paths.get("src/files/ingredients.txt"));
+            System.out.println(lines.size());
+            return lines.stream()
+                    .map(line -> new Ingredient(line.split(";")[0],
+                            Short.parseShort(line.split(";")[1]),
+                            line.split(";")[2],
+                            Float.parseFloat(line.split(";")[3]),
+                            Float.parseFloat(line.split(";")[4]),
+                            Float.parseFloat(line.split(";")[5]),
+                            Float.parseFloat(line.split(";")[6]),
+                            Float.parseFloat(line.split(";")[7]),
+                            Float.parseFloat(line.split(";")[8]),
+                            Float.parseFloat(line.split(";")[9]),
+                            Float.parseFloat(line.split(";")[10]))
+                    )
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            return new ArrayList<>();
+        }
     }
 }
