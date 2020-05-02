5b281121c01109723043613dc6fa90786c244192
Pablo <pablonisen@gmail.com>
2020-05-02 18:53:22 +0200

    New method addIngredient

--- Stage_02/CookApp/src/scenes/pantry/pantryController.java
+++ Stage_02/CookApp/src/scenes/pantry/pantryController.java
@@ -7,12 +7,16 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.util.Pair;
 import objects.Ingredient;
 import objects.StockIngredient;
 
@@ -185,13 +189,117 @@ public class pantryController implements Initializable
 
     public void addIngredient(ActionEvent actionEvent)
     {
-        //TODO
+
+        // Create the custom dialog.
+        Dialog<Pair<String, String>> dialog = new Dialog<>();
+        dialog.setTitle("New ingredient");
+        dialog.setHeaderText("Add a new ingredient to your pantry");
+
+        // Set the icon (must be included in the project).
+        dialog.setGraphic(new ImageView(this.getClass().getResource("../../files/img/apple.png").toString()));
+
+        // Set the button types.
+        ButtonType addButton = new ButtonType("Add", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(addButton, ButtonType.CANCEL);
+
+        // Create labels and fields.
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(20, 150, 10, 10));
+
+        TextField amount = new TextField();
+        amount.setPromptText("amount");
+
+        ChoiceBox<String> choiceBox = new ChoiceBox<>();
+        choiceBox.setPrefWidth(150);
+        amount.setPrefWidth(150);
+        amountListUpdated = FXCollections.observableArrayList(readStock());
+
+        for(int i = 0; i < ingredients.size(); i++)
+        {
+            if(amountListUpdated.get(i) == 0)
+                choiceBox.getItems().add(ingredients.get(i).getName());
+        }
+
+        grid.add(new Label("Ingredient:"), 0, 0);
+        grid.add(choiceBox, 1, 0);
+        grid.add(new Label("Amount:"), 0, 1);
+        grid.add(amount, 1, 1);
+
+        // Enable/Disable login button depending on whether an amount was entered.
+        Node activeButton = dialog.getDialogPane().lookupButton(addButton);
+        activeButton.setDisable(true);
+
+        // Do some validation (using the Java 8 lambda syntax).
+        amount.textProperty().addListener((observable, oldValue, newValue) -> {
+            activeButton.setDisable(newValue.trim().isEmpty());
+        });
+
+        // force the field to be numeric only
+        amount.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    amount.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+
+        dialog.getDialogPane().setContent(grid);
+
+        // Convert the result when the button is clicked.
+        dialog.setResultConverter(dialogButton -> {
+            if (dialogButton == addButton) {
+                return new Pair<>(choiceBox.getValue(), amount.getText());
+            }
+            return null;
+        });
+
+        Optional<Pair<String, String>> result = dialog.showAndWait();
+        result.ifPresent(newIngredient -> {
+            addIngredientToFile(newIngredient.getKey(), Short.parseShort(newIngredient.getValue()));
+
+            for (int i = 0; i < ingredients.size(); i++)
+            {
+                if(ingredients.get(i).getName().equals(newIngredient.getKey())) {
+                    stockList.add(new StockIngredient(ingredients.get(i), Short.parseShort(newIngredient.getValue())));
+                    stockListCopy.add(new StockIngredient(ingredients.get(i), Short.parseShort(newIngredient.getValue())));
+                }
+            }
+        });
+
+    }
+
+    public void addIngredientToFile(String ingredientName, Short IngredientAmount)
+    {
+        PrintWriter listOfIngredients = null;
+        try
+        {
+            listOfIngredients = new PrintWriter("src/files/data/stock.txt");
+            for(int i = 0; i < ingredients.size(); i++)
+            {
+                if(ingredients.get(i).getName().equals(ingredientName))
+                    listOfIngredients.println(
+                            ingredients.get(i).getName() + ";" + IngredientAmount);
+                else
+                    listOfIngredients.println(ingredients.get(i).getName() + ";"
+                            + amountList.get(i));
+            }
+        }
+
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            listOfIngredients.close();
+        }
+
     }
 
     public void searchIngredient(KeyEvent keyEvent)
     {
         stockList.clear();
-
 
         for(int i = 0; i <stockListCopy.size(); i++)
         {
@@ -200,6 +308,4 @@ public class pantryController implements Initializable
                 stockList.add(stockListCopy.get(i));
         }
     }
-
-
 }
